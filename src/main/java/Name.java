a9fb8783dc9eef590be772a2ddfbe237984df6b9
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-21 15:54:30 +0100

    output working

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -1,2 +1,16 @@
 public class Name {
+
+    private String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
